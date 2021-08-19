57c2d358e7f2b517733c37a88a30aeb6918b986c
luisgbyte <44074498+luisgbyte@users.noreply.github.com>
2021-08-19 15:12:27 -0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'afdb5cda168abb0ab1e684255b705aae'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b3c4fe8c-3d11-47d6-9061-240c72f7e6e5'
+};
